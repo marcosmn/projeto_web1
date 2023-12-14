ae5af0fadcb017b7e841fbc9842c462aa5a7cefd
marcosmn <marcosmnobrega@gmail.com>
2023-12-14 11:51:07 -0300

    Organizando js e criando service base

---
+++ js/baseService.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "admin",
+    password: "123456"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+
+    // QUERY
+    con.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
