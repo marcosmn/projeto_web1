bfd236eb38ba7e4c381d08f636d90a5abaac9eda
Pedro Henrique <pedrosales2806@gmail.com>
2023-12-14 18:20:38 -0300

    Ajustes na informação mostrada no perfil

---
+++ js/perfil.js
@@ -0,0 +1,14 @@
+var usuarioDados = localStorage.getItem('usuario');
+
+if(usuarioDados){
+    var objeto = JSON.parse(usuarioDados);
+    var nomeUsuario = document.querySelector("#nome-usuario");
+    var emailUsuario = document.querySelector("#e-mail");    
+    
+    nomeUsuario.innerHTML = objeto.nome;
+    emailUsuario.innerHTML = objeto.email;
+}
+else{
+    console.log("Não existe");
+}
+
