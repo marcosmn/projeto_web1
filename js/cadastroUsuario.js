e7737c7e9a51bc621acc15dbd865a6a950b0bc0e
Pedro Henrique <pedrosales2806@gmail.com>
2023-12-14 18:49:11 -0300

    Ajuste nos campos do cadastro

--- js/cadastroUsuario.js
+++ js/cadastroUsuario.js
@@ -1,16 +1,16 @@
 function cadastrar(){
     // Obter os valores dos campos do formulário
     var nome = document.getElementById('name').value;
+    var senha = document.getElementById('password').value;
     var contato = document.getElementById('contact').value;
     var email = document.getElementById('email').value;
-    var outros = document.getElementById('other').value;
 
     // Criar um objeto para armazenar os dados do usuário
     var usuario = {
         nome: nome,
+        senha: senha,
         contato: contato,
-        email: email,
-        outros: outros
+        email: email
     };
 
     // Armazena os dados localmente
