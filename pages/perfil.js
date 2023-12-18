984548ce19c1733f5d318a3c5638caec0f7fe36a
Pedro Henrique <pedrosales2806@gmail.com>
2023-12-17 21:50:03 -0300

    Logica com criação do localStore (vai mudar)

--- pages/perfil.js
+++ pages/perfil.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useEffect } from 'react';
 
 export default function Perfil(){
     const styles = {
@@ -84,9 +85,54 @@ export default function Perfil(){
         
     }
 
-    function voltar(){
-            
-    }
+    useEffect(() => {
+        // Código que será executado após a renderização inicial da página
+        console.log('Página carregada!');
+        localStorage.setItem("usuario", JSON.stringify({ nome: "pedro", email: "pedro123@gmail" }))
+
+        var usuarioDados = localStorage.getItem("usuario");
+
+        if(usuarioDados){
+            var usuario = JSON.parse(usuarioDados);
+            var nomeUsuario = document.querySelector("#nome-usuario");
+            var emailUsuario = document.querySelector("#e-mail");    
+
+            nomeUsuario.innerHTML = usuario.nome;
+            emailUsuario.innerHTML = usuario.email;
+        }
+        else{
+            console.log("Não existe");
+        }
+
+        var principaisAcoes = localStorage.getItem("acao");
+
+        if(principaisAcoes){
+            var acao = JSON.parse(principaisAcoes);
+            var codAcao = acao.codigoAcao;
+            var nomeEmpresa = acao.nomeEmpresa;
+
+            // Criar um novo elemento
+            var novoElemento = document.createElement("h3");
+
+            // Adicionar conteúdo ao elemento
+            novoElemento.textContent = `${codAcao} : ${nomeEmpresa}`;
+
+            // Encontrar o elemento existente no DOM
+            var elementoExistente = document.getElementById("principais-ativos");
+
+            // Adicionar o novo elemento como filho do elemento existente
+            elementoExistente.appendChild(novoElemento);
+
+        }
+        else{
+            console.log("Não há ações");
+        }
+
+      }, []);
+
+
+
+
         return (
             <> 
                 <header style={styles.header}>
